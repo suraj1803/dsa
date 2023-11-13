69dc8359281be9b367828c65a007c642f7f29e0c
Suraj Biswas <surajbiswas182003@gmail.com>
2023-11-13 23:39:38 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import mathemaics.MathQ;
 public class Main {
     public static void main(String[] args) {
         MathQ math = new MathQ();
-        math.allDivisors(25);
+        math.allDivisors(100);
     }
 }
--- src/mathemaics/MathQ.java
+++ src/mathemaics/MathQ.java
@@ -54,13 +54,25 @@ public class MathQ {
     }
 
     public void allDivisors(int n) {
-        for (int i = 1; i * i <= n; i++) {
-            if (n % i == 0) {
-                System.out.println(i);
-                if (i != n / i)
-                    System.out.println(n / i);
+        // without sorting
+//        for (int i = 1; i * i <= n; i++) {
+//            if (n % i == 0) {
+//                System.out.println(i);
+//                if (n / i != i)
+//                    System.out.println(n / i);
+//            }
+//        }
 
-            }
+        // with sorting
+        int i = 1;
+        for (i = 1; i * i <= n; i++) {
+            if (n % i == 0)
+                System.out.println(i);
+        }
+
+        for ( ; i >= 1; i--) {
+            if (n % i == 0 && n / i != i)
+                System.out.println(n / i);
         }
     }
 }
