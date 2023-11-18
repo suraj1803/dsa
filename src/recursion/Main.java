0553c6804c1796cbb67ab5c52a3d5a8e3134fc51
Suraj Biswas <surajbiswas182003@gmail.com>
2023-11-19 01:19:12 +0530

    added

--- src/recursion/Main.java
+++ src/recursion/Main.java
@@ -6,6 +6,6 @@ public class Main {
 //        System.out.println(recursionQ.maxPieces(5, 2, 3, 4));
         RecursionOnArray  r = new RecursionOnArray();
 //        r.print(new int[]{1, 2, 3, 4, 5}, 0);
-        System.out.println(r.max(new int[]{3, 10, 2, 4}, 0));
+        System.out.println(r.sum(new int[]{3, 10, 2, 4}, 0));
     }
 }
--- src/recursion/RecursionOnArray.java
+++ src/recursion/RecursionOnArray.java
@@ -9,8 +9,14 @@ public class RecursionOnArray {
     }
 
     public int max(int[] arr, int index) {
-        if (index == arr.length)
-            return arr[index - 1];
+        if (index == arr.length - 1)
+            return arr[index];
         return Math.max(arr[index], max(arr, index + 1));
+    }
+
+    public int sum(int[] arr, int index) {
+        if (index == arr.length - 1)
+            return arr[index];
+        return arr[index] + sum(arr, index + 1);
     }
 }
