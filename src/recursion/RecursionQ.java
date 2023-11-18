ab8c0389e41291e5b5c1df682f737b60260294ae
Suraj Biswas <surajbiswas182003@gmail.com>
2023-11-18 23:13:44 +0530

    fixed power function

--- src/recursion/RecursionQ.java
+++ src/recursion/RecursionQ.java
@@ -56,8 +56,8 @@ public class RecursionQ {
     }
 
     public int power(int p, int q) {
-        if (q == 1)
-            return p;
+        if (q == 0)
+            return 1;
         return p * power(p , q - 1);
     }
 
