60ba47ebada76483400007fb87a2879a38b16d36
Suraj Biswas <surajbiswas182003@gmail.com>
2023-11-20 09:06:37 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         PatternQ q = new PatternQ();
 //        q.halfPyramidOfNum(5);
-        q.hollowRectangle(3, 4);
+//        q.hollowRectangle(3, 4);
+//        q.invertedAndRotatedHalfPyramid(4);
+//        q.invertedHalfPyramid(5);
+        q.floydTriangle(5);
     }
 }
--- src/patterns/PatternQ.java
+++ src/patterns/PatternQ.java
@@ -34,4 +34,38 @@ public class PatternQ {
             System.out.println();
         }
     }
+
+    public void invertedAndRotatedHalfPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            // printing spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //printing stars
+            for (int  j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void invertedHalfPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public void floydTriangle(int n) {
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int  j = 1; j <= i; j++) {
+                System.out.print(num++ + " ");
+            }
+            System.out.println();
+        }
+    }
 }
