8fd86b05d9296defd086d2df06e38249169cf525
Suraj Biswas <surajbiswas182003@gmail.com>
2023-11-20 08:35:31 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import mathemaics.MathQ;
 import patterns.PatternQ;
-
-import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
--- src/patterns/PatternQ.java
+++ src/patterns/PatternQ.java
@@ -27,9 +27,9 @@ public class PatternQ {
         for (int i = 1; i <= r; i++) {
            for (int j = 1; j <= c; j++) {
                if (i == 1 || i == r || j == 1 || j == c)
-                   System.out.print("*");
+                   System.out.print(" * ");
                else
-                   System.out.print(" ");
+                   System.out.print("   ");
            }
             System.out.println();
         }
