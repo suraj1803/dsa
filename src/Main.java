41f7dc16a097133dbab1ba668d3ee7433a2dd331
Suraj Biswas <surajbiswas182003@gmail.com>
2023-11-20 23:43:57 +0530

    added diamond pattern

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import patterns.PatternQ;
 public class Main {
     public static void main(String[] args) {
         PatternQ q = new PatternQ();
-        q.butterfly(10);
+//        q.butterfly(10);
+//        q.hollowRhombus(5);
+        q.diamond(4);
     }
 }
--- src/patterns/PatternQ.java
+++ src/patterns/PatternQ.java
@@ -116,4 +116,57 @@ public class PatternQ {
             System.out.println();
         }
     }
+
+    public void solidRhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("   ");
+            }
+
+            for (int j = 1; j <= n; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void hollowRhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("   ");
+            }
+
+            for (int j = 1; j <= n; j++) {
+                if ((i == 1 || i == n) || (j == 1 || j == n)) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void diamond(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("   ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1 ; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("   ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
 }
