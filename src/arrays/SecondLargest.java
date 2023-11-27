eabddaca64967b9d4375efcee230080a179787a7
Suraj Biswas <surajbiswas182003@gmail.com>
2023-11-27 09:56:01 +0530

    added

--- src/arrays/SecondLargest.java
+++ src/arrays/SecondLargest.java
@@ -2,19 +2,21 @@ package arrays;
 
 public class SecondLargest {
     public static void main(String[] args) {
-        System.out.println(getSecondLargest(new int[]{10, 5, 3, 4, 5}));
+        System.out.println(getSecondLargest(new int[]{5, 20, 3, 10}));
     }
 
-    public static int getSecondLargest(int[] arr) {
-        int largestIndex = LargestInArray.largest(arr);
-        int secondLargestIndex = -1;
+    private static int getSecondLargest(int[] arr) {
+        int largest = LargestInArray.largest(arr);
+        int secondLargest = -1;
+
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != arr[largestIndex]) {
-                if (secondLargestIndex == -1 || arr[i] > arr[secondLargestIndex])
-                    secondLargestIndex = i;
+            if (arr[i] != arr[largest]) {
+                if (secondLargest == -1 || arr[i] > arr[secondLargest])
+                    secondLargest = i;
             }
         }
-        return secondLargestIndex;
+        return secondLargest;
     }
+
 }
 
