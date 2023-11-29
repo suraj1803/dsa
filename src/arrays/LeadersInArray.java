e73c4a755f7b4fe858e66a5c5a96891f1344bd21
Suraj Biswas <surajbiswas182003@gmail.com>
2023-11-29 17:14:05 +0530

    added

---
+++ src/arrays/LeadersInArray.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class LeadersInArray {
+    public static void main(String[] args) {
+        leaders(new int[]{7, 10, 4, 3, 6, 5, 2});
+    }
+
+    public static void leaders(int[] arr) {
+        int leader = arr[arr.length - 1];
+        System.out.print(leader + " ");
+        for (int i = arr.length - 1; i >= 0; i--) {
+                if (arr[i] > leader) {
+                    leader = arr[i];
+                    System.out.print(leader + " ");
+                }
+            }
+        }
+    }
