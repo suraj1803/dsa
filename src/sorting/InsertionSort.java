2e01df840afc15b2f7ce9f511c2ab4c85bab244b
Suraj Biswas <surajbiswas182003@gmail.com>
2023-12-24 11:51:28 +0530

    added

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(arr));
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[j + 1])
+                    break;
+                else if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
